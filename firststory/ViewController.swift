4ee8907d95d67c11ad6aca26c21467fc23ebcbe9
Sandeep <sandeepreddy4366@gmail.com>
2024-02-10 18:01:11 +0530

    Initial Commit

---
+++ firststory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firststory
+//
+//  Created by Sandeep Reddy on 10/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
